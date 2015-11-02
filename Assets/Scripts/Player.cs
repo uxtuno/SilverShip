2048c736102b0c44c3d08d89eb0e00cf8a90172e
uxtuno <uxtuno664@gmail.com>
2015-11-03 03:51:18 +0900

    ハイジャンプを実装

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,11 @@
 
 namespace Uxtuno
 {
+	/// <summary>
+	/// TODO :
+	/// 状態遷移プログラミングにより、各状態の処理はすべて状態メソッドの中に記述している
+	/// それにより重複部分が多くなっているので、後でコードを見直す必要あり
+	/// </summary>
 	public class Player : MyMonoBehaviour
 	{
 		[Tooltip("歩く速さ(単位:m/s)"), SerializeField]
@@ -17,6 +22,7 @@ namespace Uxtuno
 
 		private float jumpVY = 0.0f;
 		private float jumpPower;
+		private float highJumpPower;
 
 		private CharacterController characterController;
 		private CameraController cameraController;
@@ -42,7 +48,8 @@ namespace Uxtuno
 		{
 			None,
 			Normal, // 待機、移動
-			JumpPossible, // ジャンプ入力可能状態
+			Depression, // 踏み込み中
+            JumpPossible, // ジャンプ入力可能状態
 			Jumping, // ジャンプ中
 			HighJumping, // ハイジャンプ中
 			Fall, // 落下中
@@ -57,6 +64,7 @@ namespace Uxtuno
 		{
 			// 指定の高さまで飛ぶための初速を計算
 			jumpPower = Mathf.Sqrt(2.0f * -Physics.gravity.y * jumpHeight);
+			highJumpPower = Mathf.Sqrt(2.0f * -Physics.gravity.y * highJumpHeight);
 			characterController = GetComponent<CharacterController>();
 			characterController.detectCollisions = false;
 			cameraController = GetComponentInChildren<CameraController>();
@@ -75,23 +83,52 @@ namespace Uxtuno
 		void Update()
 		{
 			//Move(); // プレイヤーの移動など
+			moveVector = Vector3.zero;
 
-			switch(currentState)
+			do {
+				oldState = currentState;
+				switch (currentState)
+				{
+					case State.Normal:
+						Normal();
+						break;
+					case State.JumpPossible:
+						jumpPossibe();
+						break;
+					case State.Depression:
+						Depression();
+                        break;
+					case State.Jumping:
+						Jumping();
+						break;
+					case State.HighJumping:
+						HighJumping();
+						break;
+					case State.Fall:
+						Fall();
+						break;
+					case State.Attack:
+						break;
+				}
+
+				if(oldState != currentState)
+				{
+					count = 0;
+				}
+			} while (oldState != currentState);
+
+			if (moveVector != Vector3.zero)
 			{
-				case State.Normal:
-					Normal();
-					break;
-				case State.JumpPossible:
-					jumpPossibe();
-                    break;
-				case State.Jumping:
-					break;
-				case State.HighJumping:
-					break;
-				case State.Fall:
-					break;
-				case State.Attack:
-					break;
+				Vector3 oldCameraPosition = cameraController.cameraTransform.position;
+				// プレイヤー移動前
+				Vector3 old = transform.position - oldCameraPosition;
+				characterController.Move(moveVector * Time.deltaTime);
+				// プレイヤー移動後
+				Vector3 now = transform.position - oldCameraPosition;
+
+				// プレイヤーが移動した時のY軸方向カメラ回転量を計算
+				float rotateAngleY = Mathf.Atan2(now.x * old.z - now.z * old.x, now.x * old.x + now.z * old.z) * Mathf.Rad2Deg / 2.0f;
+				cameraController.CameraMove(rotateAngleY, 0.0f);
 			}
 
 			if (cameraController.targetToDistance < 0.2f)
@@ -123,7 +160,6 @@ namespace Uxtuno
 				speed = maxSpeed;
 			}
 
-			moveVector = Vector3.zero;
 			if (direction != Vector3.zero)
 			{
 				//float distance = (cameraController.transform.position - cameraController.cameraTransform.position).magnitude;
@@ -149,11 +185,188 @@ namespace Uxtuno
 				// ジャンプ可能状態へ移行
 				currentState = State.JumpPossible;
 			}
+			else
+			{
+				// ジャンプさせる
+				if (input.jump && !animator.GetBool(isJumpId))
+				{
+					jumpVY = jumpPower;
+					animator.SetBool(isJumpId, true);
+					currentState = State.Depression;
+				}
+				else
+				{
+					animator.SetBool(isJumpId, false);
+					jumpVY = 0.0f;
+				}
+			}
 		}
 
 		private void jumpPossibe()
 		{
+			if (count++ >= 3)
+			{
+				currentState = State.Fall;
+			}
 
+			Vector3 direction = Vector3.zero;
+			// directionは進行方向を表すので上下入力はzに格納
+			direction.x = Input.GetAxisRaw(InputName.Horizontal);
+			direction.z = Input.GetAxisRaw(InputName.Vertical);
+			direction.Normalize();
+
+			float speed = 3.0f;
+			if (direction != Vector3.zero)
+			{
+				//float distance = (cameraController.transform.position - cameraController.cameraTransform.position).magnitude;
+
+				Vector3 rotateAngles = Vector3.zero;
+				// カメラの方向を加味して進行方向を計算
+				direction = cameraController.cameraTransform.rotation * direction;
+
+				//// xz平面の進行方向から、Y軸回転角を得る
+				//rotateAngles.y = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+				//playerMesh.eulerAngles = rotateAngles;
+
+				moveVector = direction * speed;
+			}
+
+			jumpVY += Physics.gravity.y * Time.deltaTime;
+			if (characterController.isGrounded)
+			{
+				// 通常状態へ移行
+				currentState = State.Normal;
+				jumpVY = 0.0f;
+			}
+
+			PlayerInput input = PlayerInput.instance;
+			// ジャンプさせる
+			if (input.jump && !animator.GetBool(isJumpId))
+			{
+				animator.SetBool(isJumpId, true);
+				currentState = State.Depression;
+			}
+			_moveVector.y = jumpVY;
+		}
+
+		private void Depression()
+		{
+			PlayerInput input = PlayerInput.instance;
+			if(count < 6)
+			{
+				++count;
+				if(input.attack)
+				{
+					jumpVY = highJumpPower;
+					currentState = State.HighJumping;
+				}
+			}
+			else
+			{
+				jumpVY = jumpPower;
+				currentState = State.Jumping;
+			}
+		}
+
+		private void Jumping()
+		{
+			Vector3 direction = Vector3.zero;
+			// directionは進行方向を表すので上下入力はzに格納
+			direction.x = Input.GetAxisRaw(InputName.Horizontal);
+			direction.z = Input.GetAxisRaw(InputName.Vertical);
+			direction.Normalize();
+
+			float speed = 3.0f;
+			if (direction != Vector3.zero)
+			{
+				//float distance = (cameraController.transform.position - cameraController.cameraTransform.position).magnitude;
+
+				Vector3 rotateAngles = Vector3.zero;
+				// カメラの方向を加味して進行方向を計算
+				direction = cameraController.cameraTransform.rotation * direction;
+
+				//// xz平面の進行方向から、Y軸回転角を得る
+				//rotateAngles.y = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+				//playerMesh.eulerAngles = rotateAngles;
+
+				moveVector = direction * speed;
+			}
+
+			_moveVector.y = jumpVY;
+			jumpVY += Physics.gravity.y * Time.deltaTime;
+
+			if (jumpVY < 0.0f)
+			{
+				// 落下状態へ移行
+				currentState = State.Fall;
+			}
+		}
+
+		private void HighJumping()
+		{
+			Vector3 direction = Vector3.zero;
+			// directionは進行方向を表すので上下入力はzに格納
+			direction.x = Input.GetAxisRaw(InputName.Horizontal);
+			direction.z = Input.GetAxisRaw(InputName.Vertical);
+			direction.Normalize();
+
+			float speed = 3.0f;
+			if (direction != Vector3.zero)
+			{
+				//float distance = (cameraController.transform.position - cameraController.cameraTransform.position).magnitude;
+
+				Vector3 rotateAngles = Vector3.zero;
+				// カメラの方向を加味して進行方向を計算
+				direction = cameraController.cameraTransform.rotation * direction;
+
+				//// xz平面の進行方向から、Y軸回転角を得る
+				//rotateAngles.y = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+				//playerMesh.eulerAngles = rotateAngles;
+
+				moveVector = direction * speed;
+			}
+
+			_moveVector.y = jumpVY;
+			jumpVY += Physics.gravity.y * Time.deltaTime;
+
+			if (jumpVY < 0.0f)
+			{
+				// 落下状態へ移行
+				currentState = State.Fall;
+			}
+		}
+
+		private void Fall()
+		{
+			Vector3 direction = Vector3.zero;
+			// directionは進行方向を表すので上下入力はzに格納
+			direction.x = Input.GetAxisRaw(InputName.Horizontal);
+			direction.z = Input.GetAxisRaw(InputName.Vertical);
+			direction.Normalize();
+
+			float speed = 3.0f;
+			if (direction != Vector3.zero)
+			{
+				//float distance = (cameraController.transform.position - cameraController.cameraTransform.position).magnitude;
+
+				Vector3 rotateAngles = Vector3.zero;
+				// カメラの方向を加味して進行方向を計算
+				direction = cameraController.cameraTransform.rotation * direction;
+
+				//// xz平面の進行方向から、Y軸回転角を得る
+				//rotateAngles.y = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+				//playerMesh.eulerAngles = rotateAngles;
+
+				moveVector = direction * speed;
+			}
+
+			_moveVector.y = jumpVY;
+			jumpVY += Physics.gravity.y * Time.deltaTime;
+			if (characterController.isGrounded)
+			{
+				currentState = State.Normal;
+				jumpVY = 0.0f;
+			}
 		}
 
 		void Move()
