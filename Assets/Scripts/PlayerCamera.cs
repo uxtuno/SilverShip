a60fedb7b1102de8b8cb981088c7c30ca670cfdf
uxtuno <uxtuno664@gmail.com>
2015-11-05 10:08:28 +0900

    ワーニング削除

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -9,14 +9,13 @@ namespace Uxtuno
 		private float horizontalRotationSpeed = 100.0f; // 水平方向へのカメラ移動速度
 		[Tooltip("垂直方向のカメラ移動速度"), SerializeField]
 		private float verticaltalRotationSpeed = 40.0f; // 垂直方向へのカメラ移動速度
-		[Tooltip("水平方向のカメラ回転閾値"), SerializeField]
-		private float horizontalRotationThreshold = 0.2f; // 水平方向のカメラ回転閾値
-		[Tooltip("垂直方向のカメラ回転閾値"), SerializeField]
-		private float verticalRotationThreshold = 0.2f; // 垂直方向のカメラ回転閾値
-		[Tooltip("ドラッグの増幅倍率"), SerializeField]
-		private float cameraDragAmplification = 4.0f;
+		//[Tooltip("水平方向のカメラ回転閾値"), SerializeField]
+		//private float horizontalRotationThreshold = 0.2f; // 水平方向のカメラ回転閾値
+		//[Tooltip("垂直方向のカメラ回転閾値"), SerializeField]
+		//private float verticalRotationThreshold = 0.2f; // 垂直方向のカメラ回転閾値
+		//[Tooltip("ドラッグの増幅倍率"), SerializeField]
+		//private float cameraDragAmplification = 4.0f;
 
-		private Vector2 beginCameraDragPosition; // カメラ回転のためのドラッグ開始地点
 		private CameraController cameraController;
 
 		void Start()
@@ -43,25 +42,6 @@ namespace Uxtuno
 			Vector3 position = Vector3.zero;
 			position.x = input.cameraHorizontal;
 			position.y = input.cameraVertical;
-
-			//if (Input.GetMouseButton(1))
-			//{
-			//	position *= cameraDragAmplification; // 移動量を増幅させる
-			//	if (Mathf.Abs(position.y) < verticalRotationThreshold)
-			//	{
-			//		position.y = 0.0f;
-			//	}
-
-			//	if (Mathf.Abs(position.x) < horizontalRotationThreshold)
-			//	{
-			//		position.x = 0.0f;
-			//	}
-
-			//	if (position.sqrMagnitude > 1.0f)
-			//	{
-			//		position.Normalize();
-			//	}
-			//}
 
 			cameraController.CameraMove(position.x * horizontalRotationSpeed * Time.deltaTime, position.y * verticaltalRotationSpeed * Time.deltaTime);
 		}
