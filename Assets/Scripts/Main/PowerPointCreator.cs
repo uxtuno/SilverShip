143c57032471f2d7c00ec639a50661bac36b3f15
uxtuno <uxtuno664@gmail.com>
2016-01-14 18:51:43 +0900

    結界の点を5つ以上でないように

--- Assets/Scripts/Main/PowerPointCreator.cs
+++ Assets/Scripts/Main/PowerPointCreator.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class PowerPointCreator : MonoBehaviour {
+public class PowerPointCreator : MonoBehaviour
+{
 	private GameObject powerPointPrefab;
 	private Transform powerPointParent; // 結界の点をまとめる親
+	private LinkedList<Transform> powerPointList;
+	private static readonly int powerPointMax = 5; // 結界点の同時設置可能数
 
 	void Start()
 	{
@@ -19,6 +22,19 @@ public class PowerPointCreator : MonoBehaviour {
 	{
 		GameObject powerPoint = Instantiate(powerPointPrefab, position, Quaternion.identity) as GameObject;
 		powerPoint.transform.SetParent(powerPointParent);
+
+		if (powerPointList.Count >= powerPointMax)
+		{
+			LinkedListNode<Transform> first = powerPointList.First;
+			// 最初の点をシーン上から削除
+			if (first.Value.gameObject != null)
+			{
+				Destroy(first.Value.gameObject);
+			}
+			// リスト上から削除
+			powerPointList.Remove(first);
+		}
+		powerPointList.AddLast(powerPoint.transform);
 	}
 
 	/// <summary>
@@ -26,6 +42,8 @@ public class PowerPointCreator : MonoBehaviour {
 	/// </summary>
 	public int count
 	{
+		// リスト上にnullのまま残っている可能性もあるのでここでは実際のシーン上の数を返す
+		// nullのまま残っていても5つ以上にはならないので問題ないと考える
 		get { return powerPointParent.childCount; }
 	}
 }
