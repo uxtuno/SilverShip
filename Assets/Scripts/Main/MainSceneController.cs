60c332de214493d93ae469fcb2790462070c02e4
Kuvo <ImmortalHorizon_Kuvo@yahoo.co.jp>
2016-02-08 01:53:36 +0900

    mainシーンのBGM,SEを一部実装

---
+++ Assets/Scripts/Main/MainSceneController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Kuvo
+{
+	public class MainSceneController : MonoBehaviour
+	{
+		private MainSoundCollector soundCollector { get { return this.GetSafeComponent<MainSoundCollector>(); } }
+		[SerializeField]
+		private GameObject seaObject = null;
+
+		private void Start()
+		{
+			if(seaObject)
+			{
+				if(SoundPlayerSingleton.instance.PlaySE(seaObject, soundCollector[MainSoundCollector.SoundName.SeaWave], true))
+				{
+					Debug.LogError("SeaWaveBGMの再生に失敗しました");
+				}
+			}
+
+			if (!SoundPlayerSingleton.instance.PlayBGM(soundCollector[MainSoundCollector.SoundName.BGM], true, SoundPlayerSingleton.FadeMode.FadeIn))
+			{
+				Debug.LogError("BGMの再生に失敗しました");
+			}
+		}
+	}
+}
--- Assets/Scripts/Main/MainSoundCollector.cs
+++ Assets/Scripts/Main/MainSoundCollector.cs
@@ -12,6 +12,10 @@ namespace Kuvo
 		public enum SoundName
 		{
 			NONE,
+			// 2DSounds
+			BGM,
+			// MapObjectSounds
+			SeaWave,
 			// EnemySounds
 			EnemySAttack,       // 近距離攻撃
 			EnemyLAttack,       // 遠距離攻撃
@@ -21,6 +25,8 @@ namespace Kuvo
 		protected override void Awake()
 		{
 			sounds.Add(SoundName.NONE, null);
+			sounds.Add(SoundName.BGM, Resources.Load<AudioClip>("Sounds/BackGroundMusic/MainSceneBGM") as AudioClip);
+			sounds.Add(SoundName.SeaWave, Resources.Load<AudioClip>("Sounds/BackGroundMusic/SeaWaveBGM") as AudioClip);
 			sounds.Add(SoundName.EnemySAttack, Resources.Load<AudioClip>("Sounds/SoundEffects/EnemyVoice") as AudioClip);
 			sounds.Add(SoundName.EnemyLAttack, Resources.Load<AudioClip>("Sounds/SoundEffects/EnemyVoice") as AudioClip);
 			sounds.Add(SoundName.EnemyDamage, Resources.Load<AudioClip>("Sounds/SoundEffects/EnemyDeathSE") as AudioClip);
